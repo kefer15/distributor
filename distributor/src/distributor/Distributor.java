2a34841837f2b603322079b84dc031bb5ba2ffea
kefer15 <kefer15@gmail.com>
2015-08-27 23:06:15 -0500

    Modificación de la clase Distributor en su función main

---
+++ distributor/src/distributor/Distributor.java
@@ -0,0 +1,20 @@
+package distributor;
+
+import java.util.Scanner;
+
+public class Distributor {
+
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        
+        System.out.println("OPERACIONES");
+        System.out.println(" -- > SUMA : " + a + " + " + b + " = " + (a+b));
+        System.out.println(" -- > RESTA : " + a + " + " + b + " = " + (a-b));
+        System.out.println(" -- > MULTIPLICACION : " + a + " + " + b + " = " + (a*b));
+        System.out.println(" -- > DIVISION : " + a + " + " + b + " = " + (a/b));
+    }
+}
